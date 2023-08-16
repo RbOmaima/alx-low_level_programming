1255fb6574dbc2dfd00ed4a046797837e61cca3d
RbOmaima <rbomaima100@gmail.com>
2023-08-16 05:28:09 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the string aname
+ * @f: the printing finction pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name && f)
+
+		f(name)
+}
+
